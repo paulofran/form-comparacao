c35d2804a8b3f5aee33391ed5d5e89f6e0d10eab
Paulo Francisco <paulofrancisco1300@gmail.com>
2023-12-08 06:34:20 -0300

    upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var compareButton = document.querySelector(".container button");
+
+    compareButton.addEventListener("click", function(e) {
+        e.preventDefault(); // Impede o envio do formulário
+        var numA = document.getElementById('numA').value;
+        var numB = document.getElementById('numB').value;
+
+        if(numB > numA) {
+            alert("O número " + numB + " é maior que o número " + numA + ".");
+        } else if(numB < numA) {
+            alert("O número " + numA + " é maior que o número " + numB + ".");
+        } else {
+            alert("Os números " + numB + " e " + numA + " são iguais.");
+        } 
+
+        document.getElementById('form-comparacao').reset();
+    });
